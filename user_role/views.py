0f36bc2c76e00db267a462ffb5bf7b576fb08dab
RajeshA76 <rajuraj0706@gmail.com>
2021-11-01 22:06:31 +0530

    edited views.py

--- user_role/views.py
+++ user_role/views.py
@@ -121,7 +121,7 @@ class BookingView(generics.GenericAPIView):
                     serializer = self.serializer_class(data=context)
                     if serializer.is_valid():
                         serializer.save()
-                        return Response(data=serializer.data,status=status.HTTP_200_OK)
+                        return Response(status=status.HTTP_200_OK)
                 else:
                     return Response(status=status.HTTP_400_BAD_REQUEST)
               
