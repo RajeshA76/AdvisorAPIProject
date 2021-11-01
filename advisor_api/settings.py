cec50a66541b2cae284b26a16539e838696717e3
RajeshA76 <rajuraj0706@gmail.com>
2021-11-01 21:30:17 +0530

    edited settings

--- advisor_api/settings.py
+++ advisor_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import django_heroku
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -130,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
