a1a795ae21f119c41f9f4438806182a4844397fe
RajeshA76 <rajuraj0706@gmail.com>
2021-11-01 21:32:49 +0530

    edited

--- advisor_api/settings.py
+++ advisor_api/settings.py
@@ -11,12 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import django_heroku
 import os
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('ENV_SECRET_KEY')
+SECRET_KEY = os.environ.get('ENV_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
